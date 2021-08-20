aae43f9ad54394e9937cc2fcbd1b76b82f7b1a52
xiaomu <xkogij@gmail.com>
2021-08-20 14:14:16 +0900

    2021/08/20 refined

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Readme
+# URL
+https://xiaomuc.github.io/speech/
+
+# how it works
 I build this form only for iOS/iPadOS voice input use. I never test other os or other voice input functions.
 To help communicate with somebody who has hearing difficulties.
 Push the microphone button and speak. Text what you spoke will appear on the textbox.
@@ -6,3 +9,16 @@ The voice input turns off when:
 * About 1 minute no input  
 * put it off manually  
 30 seconds later, the text will be erased automatically.
+
+# options
+You can change the text size and erase timer settings.
+If you want to change the text size to 48pt and 
+the timer to 5sec (= 5000msec), set parameters like below:
+
+https://xiaomuc.github.io/speech/?size=48pt&timer=5000
+
+# this is made for my mum
+My mother got hearing difficulties. My family tried to communicate with her by writing. But it takes time to catch up with what somebody said. She gradually keeps silent and gives up join to conversation.
+She has an iPad.
+You know that iOS/iPadOS voice input has improved its speed and recognition rate. I thought it help her joining the conversation.
+At first, I tried the memo app with voice recognition. It works, but the text remains after voice input. Most of our conversation is just small things. We don't need to save them at all. But it is too hard for my mum to erase text every time.
--- speech.js
+++ speech.js
@@ -1,68 +1,86 @@
 // get url params
 var url = new URL(window.location.href);
 var params = url.searchParams;
-const inputTextarea = document.getElementById("FlexTextarea");
-const log = document.getElementById("event-log");
-const list_view = document.getElementById("list-view");
-var timer; 
-var inputArray=[];
-function writeLog(msg){
-    const date1 = new Date();
-    //log.textContent = log.textContent + date1.toLocaleTimeString()+`:` + msg + `\n`;
-    log.textContent = log.textContent + `${performance.now()}\t${msg}\n`;
+const inputTextarea = document.getElementById('FlexTextarea');
+var timer;
+var inputArray = [];
+
+/*
+const debug_div = document.getElementById('debug');
+const log = document.getElementById('event-log');
+const list_view = document.getElementById('list-view');
+
+// debug logs(not used)
+function writeLog(msg) {
+	if (debug_div.style.display == "block") {
+		const date1 = new Date();
+		log.textContent = log.textContent + `${performance.now()}\t${msg}\n`;
+	}
 }
+*/
+
+// set flexible textarae and initialize font size, timer setting
 function flexTextarea(el) {
-    sz = params.get("size");
-    writeLog("size:"+sz);
-    if(sz != null){
-        el.style.fontSize=sz;
-    }
-    tm = params.get("timer");
-    if(tm == null){
-        tm = 30000;
-    }
-    writeLog("timer:"+tm);
-    const dummy = el.querySelector('.FlexTextarea__dummy')
-    el.querySelector('.FlexTextarea__textarea').addEventListener('input', e => {
-        dummy.textContent = e.target.value + '\u200b';
-        writeLog(`${e.type}\t${e.data}`);
-        //writeLog(e.target.value);
-        inputTime = performance.now();
-        inputValue = e.data;
-        if(inputValue==null){
-            inputValue = '[null]';
-        }
+    // Font size
+    sz = params.get('size');
+	//writeLog('size:' + sz);
+	if (sz != null) {
+		el.style.fontSize = sz;
+	}
+    // Timer msec
+	tm = params.get('timer');
+	if (tm == null) {
+		tm = 30000;
+	}
+	//writeLog('timer:' + tm);
 
-        if(inputArray.length>0 && inputTime - inputArray[0].time < 5){
-            inputValue = inputArray[0].value + inputValue;
-        }
-        inputArray.unshift({
-            time:inputTime,
-            value:inputValue
-        });
-        clearTimeout(timer);
-        for(var i=0; i<inputArray.length;i++){
-            var item = inputArray[i];
-            if(inputTime-item.time>5 && inputValue==item.value){
-                timer = setTimeout(clr,tm);
-                writeLog('timer set')
-                break;
-            }
-        };
-        list_view.textContent="";
-        for(var i=0;i<inputArray.length;i++){
-            item = inputArray[i];
-            list_view.textContent = list_view.textContent + `${item.time}\t${item.value}\n`;
-        }
-    });
-}
-var clr = function(){
-    document.getElementById("text_form").reset();
-    writeLog("timer-timeout");
-    inputTextarea.focus();
-    inputArray=[];
+    // initialize
+	const dummy = el.querySelector('.FlexTextarea__dummy');
+	el.querySelector('.FlexTextarea__textarea').addEventListener('input', (e) => {
+		dummy.textContent = e.target.value + '\u200b';
+		//writeLog(`${e.type}\t${e.data}`);
+		//writeLog(e.target.value);
+		inputTime = performance.now();
+		inputValue = e.data;
+		if (inputValue == null) {
+			inputValue = '[null]';
+		}
+
+		if (inputArray.length > 0 && inputTime - inputArray[0].time < 5) {
+			inputValue = inputArray[0].value + inputValue;
+		}
+		inputArray.unshift({
+			time: inputTime,
+			value: inputValue
+		});
+		clearTimeout(timer);
+		for (var i = 0; i < inputArray.length; i++) {
+			var item = inputArray[i];
+			if (inputTime - item.time > 5 && inputValue == item.value) {
+				timer = setTimeout(clr, tm);
+				//writeLog('timer set');
+				break;
+			}
+		}
+		/*
+        list_view.textContent = '';
+		for (var i = 0; i < inputArray.length; i++) {
+			item = inputArray[i];
+			list_view.textContent = list_view.textContent + `${item.time}\t${item.value}\n`;
+		}
+        */
+	});
 }
 
-window.onload = function(){
-    document.querySelectorAll('.FlexTextarea').forEach(flexTextarea)
-}
+// clear textarea 
+var clr = function() {
+	document.getElementById('text_form').reset();
+	//writeLog('timer-timeout');
+	inputTextarea.focus();
+	inputArray = [];
+};
+
+// initialize when windows has loaded
+window.onload = function() {
+	document.querySelectorAll('.FlexTextarea').forEach(flexTextarea);
+};
